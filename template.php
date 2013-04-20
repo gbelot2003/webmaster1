b2a044709d2775207f05a5d93ac78531faeba138
gbelot2003 <gerardobelot@outlook.com>
2013-04-20 17:42:04 -0600

    modified:   css/styles.css 	modified:   template.php 	README.md 	template/field-field_imagen_portafolio.tpl.php

--- template.php
+++ template.php
@@ -7,3 +7,10 @@ function webmaster1_preprocess_page(&$variables){
 	$variables['path_logo'] = $base_url . '/' . drupal_get_path('theme', 'webmaster1') .  "/images/logo3_1.png";
 	$variables['path_mlogos'] = $base_url . '/' .drupal_get_path('theme', 'webmaster1') .  "/images";
 }
+
+function webmaster1_preprocess_image_style(&$variables)  {
+    // If this image is of the type 'Staff Photo' then assign additional classes to it:
+    if ($variables['style_name'] == 'medium' || $variables['style_name'] == 'portafolio_show') {
+        $variables['attributes']['class'][] = 'img-polaroid';
+    }
+}
